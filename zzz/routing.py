131028614c0726bf8b37609dae05596176be1df5
Robert Fialek <robert.fialek@gmail.com>
2026-07-02 01:57:12 +0200

    Suppress asyncio.CancelledError logs from Django ASGI middleware.     CancelledError is normal when clients disconnect mid-request and should not be logged as ERROR.

--- zzz/routing.py
+++ zzz/routing.py
@@ -1,3 +1,5 @@
+import asyncio
+import logging
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zzz.settings")
@@ -7,7 +9,25 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
+log = logging.getLogger(__name__)
+
+class CancelledErrorMiddleware:
+    """
+    Suppress asyncio.CancelledError logs from Django ASGI middleware.
+    CancelledError is normal when clients disconnect mid-request and should not be logged as ERROR.
+    """
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        try:
+            return await self.app(scope, receive, send)
+        except asyncio.CancelledError:
+            log.debug("ASGI request cancelled by client (normal disconnect)")
+            raise
+
 django_asgi_app = get_asgi_application()
+django_asgi_app = CancelledErrorMiddleware(django_asgi_app)
 
 from chat.consumers import ChatConsumer
 
