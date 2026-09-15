043ff76b8d8e6f6f822cc7334d30b6848a770867
Robert Fialek <robert.fialek@gmail.com>
2026-09-15 10:26:24 +0200

    testy

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ module.exports = defineConfig({
     workers: 1,
     reporter: [['list']],
     use: {
-        baseURL: process.env.E2E_BASE_URL || 'http://localhost:8000',
+        baseURL: 'http://localhost:8000',
         trace: 'retain-on-failure',
         screenshot: 'only-on-failure',
     },
