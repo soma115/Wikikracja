a7a2521c57cc52ba6c536b01af171a4f8aeb994b
Robert Fialek <robert.fialek@gmail.com>
2026-09-25 09:53:57 +0200

    Wyłączenie `django_browser_reload` i `DEBUG` w ustawieniach testowych — ustawienie `DEBUG = False` + usunięcie `django_browser_reload` z `INSTALLED_APPS` i `MIDDLEWARE`

--- zzz/test_settings.py
+++ zzz/test_settings.py
@@ -1,5 +1,9 @@
 from zzz.settings import *  # noqa: F401, F403
 
+DEBUG = False
+INSTALLED_APPS = [app for app in INSTALLED_APPS if app != 'django_browser_reload']  # noqa: F405
+MIDDLEWARE = [middleware for middleware in MIDDLEWARE if middleware != 'django_browser_reload.middleware.BrowserReloadMiddleware']  # noqa: F405
+
 DATABASES['default']['TEST'] = {'NAME': ':memory:'}  # noqa: F405
 
 CACHES = {'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}}
